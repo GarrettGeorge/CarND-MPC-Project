1dd24b1e9ab5b288c9d98914b7d1347a7e6b437a
GarrettGeorge <garrettjgeorge@gmail.com>
2019-01-06 18:02:48 +0000

    Improves MPC results and README

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -51,8 +51,8 @@ class FG_eval {
     
     // Cost based on the reference state.
     for (unsigned int t = 0; t < N; t++) {
-      fg[0] += CppAD::pow(vars[cte_start + t], 2);
-      fg[0] += 50 * CppAD::pow(vars[epsi_start + t], 2);
+      fg[0] += 10 * CppAD::pow(vars[cte_start + t], 2);
+      fg[0] += 500 * CppAD::pow(vars[epsi_start + t], 2);
       fg[0] += CppAD::pow(vars[v_start + t] - ref_v, 2);
     }
     
@@ -60,7 +60,7 @@ class FG_eval {
     for (unsigned int t = 0; t < N - 1; t++) {
       fg[0] += 150 * CppAD::pow(vars[delta_start + t], 2);
       fg[0] += 50 * CppAD::pow(vars[a_start + t], 2);
-      fg[0] += 100 * CppAD::pow(vars[v_start + t] * vars[cte_start + t], 2);
+      fg[0] += 100 * CppAD::pow(vars[a_start + t] * vars[cte_start + t], 2);
     }
     
     // Minimize the value gap between sequential actuations, smoothes ride
@@ -96,6 +96,12 @@ class FG_eval {
       // Only consider the actuation at time t.
       AD<double> delta0 = vars[delta_start + t - 1];
       AD<double> a0 = vars[a_start + t - 1];
+      
+      // To combat latency use previous actuator values
+      if (t > 1) {
+        delta0 = vars[delta_start + t - 2];
+        a0 = vars[a_start + t - 2];
+      }
       
       // Expand out to third order polynomial for increased accuracy
       AD<double> f0 = coeffs[0] + coeffs[1] * x0 + coeffs[2] * CppAD::pow(x0, 2) + coeffs[3] * CppAD::pow(x0, 3);
